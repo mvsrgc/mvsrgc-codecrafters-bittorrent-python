0b580b72cb63515e3c43be28e049aebb3bf73799
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2023-10-26 07:21:46 -0400

    Try new decoder

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 import json
 import sys
 
+from typing import Any
+from dataclasses import dataclass
+
 # import bencodepy - available if you need it!
 # import requests - available if you need it!
 
@@ -8,7 +11,45 @@ import sys
 #
 # - decode_bencode(b"5:hello") -> b"hello"
 # - decode_bencode(b"10:hello12345") -> b"hello12345"
+
+@dataclass
+class Decoder:
+    data: Any
+    idx: int
+
+    def _parse(self):
+        char = self.data[self.idx: self.idx + 1]
+        if char == b'i':
+            self.idx += 1
+            return int(self._read_to(b'e'))
+        elif char in [b'0', b'1', b'2', b'3', b'4', b'5', b'6', b'7', b'8', b'9']:
+            str_len = int(self._read_to(b':'))
+            return self._read(str_len)
+
+    def _read(self, i: int):
+        bytes = self.data[self.idx : self.idx + i]
+        self.idx += i
+
+        if len(bytes) != i:
+            raise IndexError("Incorrect byte length returned.")
+
+        return bytes
+
+    def _read_to(self, terminator: bytes):
+        try:
+            i = self.data.index(terminator, self.idx)
+            bytes = self.data[self.idx:i]
+            self.idx = i + 1
+            return bytes
+        except ValueError:
+            raise ValueError("Unable to find terminator.")
+
+    
 def decode_bencode(bencoded_value):
+    decoder = Decoder(bencoded_value, 0)
+
+    return decoder._parse()
+
     if chr(bencoded_value[0]).isdigit():
         first_colon_index = bencoded_value.find(b":")
         if first_colon_index == -1:
@@ -18,9 +59,15 @@ def decode_bencode(bencoded_value):
         if chr(bencoded_value[-1]) != 'e':
             raise ValueError("Invalid encoded integer")
         return int(bencoded_value[1:len(bencoded_value) - 1])
+    elif chr(bencoded_value[0]) == 'l':
+        return parse_list(bencoded_value, 0)
+
     else:
         raise NotImplementedError("Only strings are supported at the moment")
 
+
+def parse_list(bencoded_value, idx):
+    pass
 
 def main():
     command = sys.argv[1]
